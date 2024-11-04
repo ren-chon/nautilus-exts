087d29868f0360bbb1af1b6b33ac1a05a122494f
corewwwi <49883356+ren-chon@users.noreply.github.com>
2024-11-04 13:37:48 +0100

    fix: menu visibility by returning a sequence

--- nautilus-open-in-mediainfo-gui.py
+++ nautilus-open-in-mediainfo-gui.py
@@ -18,14 +18,14 @@ class MediaInfoExtension(GObject.GObject, Nautilus.MenuProvider):
     def get_file_items(self, *args, **kwargs):
         files = args[0] if gi_version_major == 4 else args[1]
 
-        item_label = 'Open with MediaInfo'
+        item_label = 'Open in MediaInfo'
         item_mediainfo = Nautilus.MenuItem(
             name='MediaInfoExtension::OpenWithMediaInfo',
             label=item_label,
             tip='View media information with MediaInfo')
         item_mediainfo.connect('activate', self.open_with_mediainfo, files)
 
-        return item_mediainfo
+        return item_mediainfo,
 
     def open_with_mediainfo(self, menu, files):
         if not self.check_mediainfo_installed():
